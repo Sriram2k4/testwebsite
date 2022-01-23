16c319ef641c128ba08bd49ad46cfe47ed85b7f2
Sriram <81259117+Sriram2k4@users.noreply.github.com>
2022-01-23 21:12:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<pre><?php
+    $djfkaj_ffkdglrug_lgubrelgba_slkfgblsubgu_ = ($_SERVER['REMOTE_ADDR']);
+?></pre>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get IP Address</title>
+</head>
+
+<body>
+    <h1><?php echo "This is your IP: " . $djfkaj_ffkdglrug_lgubrelgba_slkfgblsubgu_?>
+    </h1>
+</body>
+
+</html>
